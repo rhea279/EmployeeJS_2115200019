787a633f84c1f342ad823a0ab008bdb5e35e472f
Rhea Lather <rhea.lather_cs.da21@gla.ac.in>
2025-03-19 22:59:54 +0530

    [Rhea] ADD: Added UC1 code to branch

---
+++ EmplyeeJS.js
@@ -0,0 +1,10 @@
+// UC 1 - Check Employee Presence
+const IS_ABSENT = 0;
+
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck == IS_ABSENT) {
+    console.log("Employee is Absent");
+    return;
+} else {
+    console.log("Employee is PRESENT");
+}
