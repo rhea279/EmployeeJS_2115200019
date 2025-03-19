8825036dbbf47f50258eb1e30352c7535c6117b2
Rhea Lather <rhea.lather_cs.da21@gla.ac.in>
2025-03-20 00:23:23 +0530

    [Rhea] ADD: Added UC10 code to branch

--- EmployeeJS.js
+++ EmployeeJS.js
@@ -133,3 +133,26 @@ empDailyHrsMap.forEach((value, key, map) => {
 console.log("Full Working Days: " + fullWorkingDays);
 console.log("Part Working Days: " + partWorkingDays);
 console.log("Non Working Days: " + nonWorkingDays);
+
+// UC 10 Object Creation
+totalEmpHrs = 0;
+totalWorkingDays = 0;
+let empDailyHrsAndWageArr = new Array();
+
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+
+    empDailyHrsAndWageArr.push({
+        dayNum: totalWorkingDays,
+        dailyHours: empHrs,
+        dailyWage: calcDailyWage(empHrs),
+        toString() {
+            return `\nDay ${this.dayNum} => Working Hours: ${this.dailyHours} And Wage Earned: ${this.dailyWage}`;
+        },
+    });
+}
+
+console.log("UC 10 Showing Daily Hours Worked and Wage Earned:" + empDailyHrsAndWageArr);
